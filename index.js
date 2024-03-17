7fdfed503123fa3ea3511029e6aae4a96556397f
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-17 22:19:40 +0300

    Done js

---
+++ index.js
@@ -0,0 +1,8 @@
+const 
+    let index = (2 + 2),
+        name = "John Doe",
+        age = 30;
+    
+    function greet(name, age) {
+        console.log("Hello, my name is " + name + ". I am " + age + " years old.");
+}
