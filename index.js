fb754e9d4dfaa83d85369acb270cce517d9605f1
james-mbuvi <152844337+james-mbuvi@users.noreply.github.com>
2024-03-17 22:31:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const 
     let index = (2 + 2),
         name = "John Doe",
-        age = 30;
+        age = 123345;
     
     function greet(name, age) {
         console.log("Hello, my name is " + name + ". I am " + age + " years old.");
